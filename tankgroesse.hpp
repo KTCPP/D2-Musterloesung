744da18a166e1ace302365c899f25eb1fa939337
Mitarbeiter <daniel.manz@karrieretutor.de>
2022-09-27 11:14:30 +0200

    kopierfehler fixed

--- tankgroesse.hpp
+++ tankgroesse.hpp
@@ -1,35 +1,15 @@
-#ifndef TANKFUELLUNG_HPP_INCLUDED
-#define TANKFUELLUNG_HPP_INCLUDED
-#include "tankgroesse.hpp"
-class Tankfuellung {
+#ifndef TANKGROESSE_HPP_INCLUDED
+#define TANKGROESSE_HPP_INCLUDED
+#include <ostream> // ostream
+class Tankgroesse {
 public:
-	explicit Tankfuellung(double stand = 0) : stand{ stand } {}
-	double stand;
-	// Überladen hier, damit es nicht zu einer zyklischen Abhängigkeit kommt
-	inline Tankfuellung& operator=(const Tankgroesse& groesse) {
-		stand = groesse.groesse;
-		return *this;
-	}
-	inline Tankfuellung& operator=(const double& groesse) {
-		stand = groesse;
-		return *this;
-	}
-	inline Tankfuellung& operator+=(const double& menge) {
-		stand += menge;
-		return *this;
-	}
-	inline Tankfuellung& operator-=(const double& menge) {
-		stand -= menge;
-		return *this;
-	}
+	explicit Tankgroesse(double groesse = 0) : groesse{ groesse } {}
+	double groesse;
 };
-inline double operator+(const Tankfuellung& tank, const double& menge) {
-	return tank.stand + menge;
+inline bool operator>(const double& menge, const Tankgroesse& groesse) {
+	return menge > groesse.groesse;
 }
-inline double operator-(Tankfuellung& tank, const double& menge) {
-	return tank.stand - menge;
+inline std::ostream& operator<<(std::ostream& os, const Tankgroesse& groesse) {
+	return os << groesse.groesse;
 }
-inline std::ostream& operator<<(std::ostream& os, const Tankfuellung& tank) {
-	return os << tank.stand;
-}
-#endif // TANKFUELLUNG_HPP_INCLUDED
+#endif // TANKGROESSE_HPP_INCLUDED
